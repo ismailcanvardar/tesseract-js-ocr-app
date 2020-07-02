be05b89d5cc7452ab8356bbe350685fd876e83f9
İsmail Can Vardar <ismailcanvardar@gmail.com>
2020-07-02 16:23:23 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { createWorker } from 'tesseract.js';
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [ocr, setOcr] = useState(null);
+
+  const worker = createWorker({
+    logger: m => console.log(m),
+  });
+
+  const doOCR = async (e) => {
+    e.preventDefault();
+    setOcr("Recognizing...");
+    const base64string = image.split(',')[1];
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(Buffer.from(base64string, 'base64'));
+    setOcr(text);
+  };
+
+  const readImage = (e) => {
+    let reader = new FileReader();
+
+    reader.onloadend = function () {
+      setImage(reader.result);
+    }
+
+    reader.readAsDataURL(e.target.files[0]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => doOCR(e)}>
+        <input type="file" multiple onChange={(e) => readImage(e)} />
+        <button type="submit">Recognize</button>
+      </form>
+
+      {
+        image && <img style={{ height: 200, width: "auto" }} src={Buffer.from(image)} alt="uploaded-image" />
+      }
+
+      {
+        ocr && (
+          <p style={{ fontSize: 40 }}>{ocr}</p>
+        )
+      }
     </div>
   );
 }
